c8fbcff85dbaa809d7124eee7ce090c6244ad8aa
bai ying <cn.baiying@gmail.com>
2026-01-01 18:03:57 +0800

    Fix bugs

--- next.config.ts
+++ next.config.ts
@@ -14,6 +14,15 @@ const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  webpack: (config) => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      fs: false,
+      net: false,
+      tls: false,
+    };
+    return config;
+  },
 };
 
 export default withNextIntl(nextConfig);
